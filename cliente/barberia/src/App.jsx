873b7b9ba55b92ff16c7c5ea0eba820e1672177b
MaxiHerrera97 <maximilianogabriel0511@gmail.com>
2026-04-20 10:39:59 -0300

    Mejoras aplicadas

--- cliente/barberia/src/App.jsx
+++ cliente/barberia/src/App.jsx
@@ -356,7 +356,7 @@ export default function App() {
   }
 
   return (
-    <div className="min-h-screen bg-zinc-950 text-zinc-100">
+    <div className="min-h-screen bg-zinc-950 text-zinc-100 notranslate" translate="no">
       {/* NAVBAR SOLO EN LANDING */}
       <Routes>
         <Route
